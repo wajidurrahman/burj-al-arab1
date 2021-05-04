da59bb17c31cf459b83884db70e1c8dd6aa489da
Wajidur Rahman <wrfuad0@gmail.com>
2021-05-04 20:14:13 +0600

    first commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,13 @@ import React, { useContext } from 'react';
 
 
 const Login = () => {
+    const handleGoogleSignIn = () =>{
+        
+    }
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn} >Google Sign in</button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9IQmXffHJMFbgBO8LCf66T02RQB7zXLw",
+    authDomain: "burj-al-arab-8f4c8.firebaseapp.com",
+    projectId: "burj-al-arab-8f4c8",
+    storageBucket: "burj-al-arab-8f4c8.appspot.com",
+    messagingSenderId: "260476072099",
+    appId: "1:260476072099:web:3900308e28b741a8f48f16"
+  };
